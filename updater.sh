a19321bff40d93d35419ca6eb37380c4a5639630
Thomas Crider <gloriouseggroll@gmail.com>
2018-03-01 11:37:27 -0500

    fix typo

--- updater.sh
+++ updater.sh
@@ -24,4 +24,3 @@ else
     wine "$EXEPREFIX/Warframe.exe" -silent -log:/Preprocessing.log -dx10:1 -dx11:1 -threadedworker:1 -cluster:public -language:en -applet:/EE/Types/Framework/ContentUpdate
     wine "$EXEPREFIX/Warframe.exe" -log:/Preprocessing.log -dx10:1 -dx11:1 -threadedworker:1 -cluster:public -language:en -fullscreen:0
 fi
-wine Downloaded/Public/
