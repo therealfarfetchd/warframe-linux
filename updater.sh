53a873b8c62ad6dcde67c0c271a2088220d14f10
Thomas Crider <gloriouseggroll@gmail.com>
2018-03-02 02:06:47 -0500

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 EXEPREFIX="$WINEPREFIX/drive_c/Program Files/Warframe/Downloaded/Public"
-##TODO use curl instead of wget
+
 wget -qN http://content.warframe.com/index.txt.lzma
 unlzma -f index.txt.lzma
 
@@ -29,18 +29,28 @@ while read line
 do
     MD5SUM=${line: -32:-5}
     MD5SUM=${MD5SUM,,}
-    if [ -f "md5sums.txt" ] && grep -q $MD5SUM "md5sums.txt"; then
+    if [ -f "md5sums.txt" ] && grep -q "$MD5SUM" "md5sums.txt" && [ -f "$EXEPREFIX${line::-38}" ]; then
         :
     else
+        #if new md5sum isn't in old md5sum list,check file for old md5sum, remove it from the list, remove blank lines
+        if [ -f "$EXEPREFIX${line::-38}" ]; then
+            OLDMD5SUM=$(md5sum "$EXEPREFIX${line::-38}" | awk '{print $1}')
+            sed -i "/$OLDMD5SUM/,+1 d" md5sums.txt
+            sed -i '/^\s*$/d' md5sums.txt
+        fi
+
+        #download new file,unlzma it, move it to correct folder
         curl -s http://content.warframe.com$line --create-dirs -o "$EXEPREFIX$line"
         find "$EXEPREFIX" -name '*.lzma' -exec unlzma -f {} \;
         mv "$EXEPREFIX${line::-5}" "$EXEPREFIX${line::-38}"
+
+        #add new md5sum to md5sums list.
+        echo "$MD5SUM" >> "md5sums.txt"
     fi
     (( LINECOUNT+=1 ))
     if (( LINECOUNT > PROGRESS )) && (( PERCENT < 101 )); then
         (( PERCENT+=1 ))
-        ##TODO: Add a progress bar
-        echo -ne "$PERCENT%"'\r';
+        echo -ne "$PERCENT% Downloading $EXEPREFIX${line::-38}                                   " "\r";
         LINECOUNT=0
     fi
 done < index.txt
@@ -48,7 +58,6 @@ done < index.txt
 cp updater.sh "$WINEPREFIX/drive_c/Program Files/Warframe/updater.exe"
 
 if [ "$WINEARCH" = "win64" ]; then
-    wine "$EXEPREFIX/Warframe.x64.exe" -silent -log:/Preprocessing.log -dx10:1 -dx11:1 -threadedworker:1 -cluster:public -language:en -applet:/EE/Types/Framework/ContentUpdate
 
     echo "*********************"
     echo "Launching Warframe."
@@ -56,7 +65,6 @@ if [ "$WINEARCH" = "win64" ]; then
 
     wine "$EXEPREFIX/Warframe.x64.exe" -log:/Preprocessing.log -dx10:1 -dx11:1 -threadedworker:1 -cluster:public -language:en -fullscreen:0
 else
-    wine "$EXEPREFIX/Warframe.exe" -silent -log:/Preprocessing.log -dx10:1 -dx11:1 -threadedworker:1 -cluster:public -language:en -applet:/EE/Types/Framework/ContentUpdate
 
     echo "*********************"
     echo "Launching Warframe."
