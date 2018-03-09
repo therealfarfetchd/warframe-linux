f77b0b105c47dcc1d25c9c02ae88494a0201f82e
Thomas Crider <gloriouseggroll@gmail.com>
2018-03-08 20:40:24 -0500

    change index url, switch back to curl, fix md5sum string

--- updater.sh
+++ updater.sh
@@ -4,7 +4,9 @@ EXEPREFIX="$WINEPREFIX/drive_c/Program Files/Warframe/Downloaded/Public"
 
 WINECMD=${WINE-wine}
 
-wget -qN http://content.warframe.com/index.txt.lzma
+#keep wget as a backup in case curl fails
+#wget -qN http://origin.warframe.com/index.txt.lzma
+curl -s http://origin.warframe.com/index.txt.lzma -o "$WINEPREFIX/drive_c/Program Files/Warframe/index.txt.lzma"
 unlzma -f index.txt.lzma
 
 echo "*********************"
@@ -27,51 +29,69 @@ PROGRESS=$((${PROGRESS::-10}/100))
 LINECOUNT=0
 PERCENT=0
 
-while read line
-do
-    MD5SUM=${line: -32:-5}
+while read -r line; do
+
+    #get the md5 sum from the current line
+    MD5SUM=${line: -37:-5}
+    #convert it to lower case
     MD5SUM=${MD5SUM,,}
+
+    #check if md5sums.txt exists
     if [ -f "md5sums.txt" ]; then
-        if `grep -q "$MD5SUM" "md5sums.txt"`; then
-            if [ -f "$EXEPREFIX${line::-38}" ]; then
-                # if the new line md5 sum is in the current md5 sum list and the file exists, do nothing
-                :
-            else
-                # if the new line md5 sum is in the list, but doesnt exist, download it
-                wget -x -O "$EXEPREFIX$line" http://content.warframe.com$line
+        #if md5sums.txt exists, check if new md5sum is in it
+        if grep -q "$MD5SUM" "md5sums.txt"; then
+
+            #if it's in the list, check if the file exists already
+            if [ ! -f "$EXEPREFIX${line:0:-38}" ]; then
+
+                # if file doesnt exist, download it
+                #keep wget as a backup in case curl fails
+                #wget -x -O "$EXEPREFIX$line" http://content.warframe.com$line
+                curl -s http://content.warframe.com$line --create-dirs -o "$EXEPREFIX$line"
                 find "$EXEPREFIX" -name '*.lzma' -exec unlzma -f {} \;
-                mv "$EXEPREFIX${line::-5}" "$EXEPREFIX${line::-38}"
+                mv "$EXEPREFIX${line:0:-5}" "$EXEPREFIX${line:0:-38}"
 
             fi
         else
-            #if new md5sum isn't in old md5sum list,check file for old md5sum, remove it from the list, remove blank lines
-            if [ -f "$EXEPREFIX${line::-38}" ]; then
-                OLDMD5SUM=$(md5sum "$EXEPREFIX${line::-38}" | awk '{print $1}')
+            #if new md5sum isn't in md5sum list,check if the file exists
+            if [ -f "$EXEPREFIX${line:0:-38}" ]; then
+                #if the file already exists, get its current md5 sum
+                OLDMD5SUM=$(md5sum "$EXEPREFIX${line:0:-38}" | awk '{print $1}')
+
+                #then remove it from the md5sum list
                 sed -i "/$OLDMD5SUM/,+1 d" md5sums.txt
+
+                #also remove blank lines
                 sed -i '/^\s*$/d' md5sums.txt
             fi
 
-            wget -x -q -O "$EXEPREFIX$line" http://content.warframe.com$line
+            #then download new file
+            #keep wget as a backup in case curl fails
+            #wget -x -q -O "$EXEPREFIX$line" http://content.warframe.com$line
+            curl -s http://content.warframe.com$line --create-dirs -o "$EXEPREFIX$line"
             find "$EXEPREFIX" -name '*.lzma' -exec unlzma -f {} \;
-            mv "$EXEPREFIX${line::-5}" "$EXEPREFIX${line::-38}"
+            mv "$EXEPREFIX${line:0:-5}" "$EXEPREFIX${line:0:-38}"
 
-            #add new md5sum to md5sums list.
+            #and add new md5sum to md5sums.txt
             echo "$MD5SUM" >> "md5sums.txt"
         fi
     else
-        #if no md5sum list exists, download all files and log md5sums
-        #download new file,unlzma it, move it to correct folder
-        wget -x -q -O "$EXEPREFIX$line" http://content.warframe.com$line
-        find "$EXEPREFIX" -name '*.lzma' -exec unlzma -f {} \;
-        mv "$EXEPREFIX${line::-5}" "$EXEPREFIX${line::-38}"
 
-        #add new md5sum to md5sums list.
+        #if no md5sum list exists, download all files and log md5sums
+        #keep wget as a backup in case curl fails
+        #wget -x -q -O "$EXEPREFIX$line" http://content.warframe.com$line
+        curl -s http://content.warframe.com$line --create-dirs -o "$EXEPREFIX$line"
+        find "$EXEPREFIX" -name '*.lzma' -exec unlzma -f {} \;
+        mv "$EXEPREFIX${line:0:-5}" "$EXEPREFIX${line:0:-38}"
         echo "$MD5SUM" >> "md5sums.txt"
+
     fi
+
+    #show progress percentage
     (( LINECOUNT+=1 ))
     if (( LINECOUNT > PROGRESS )) && (( PERCENT < 101 )); then
         (( PERCENT+=1 ))
-        echo -ne "$PERCENT% Downloading $EXEPREFIX${line::-38}                                   " "\r";
+        echo -ne "$PERCENT% Downloading $EXEPREFIX${line:0:-38}                                   " "\r";
         LINECOUNT=0
     fi
 done < index.txt
@@ -80,7 +100,7 @@ done < index.txt
 rm index.*
 
 if [ "$WINEARCH" = "win64" ]; then
-    $WINECMD "$EXEPREFIX/Warframe.x64.exe" -silent -log:/Preprocessing.log -dx10:1 -dx11:1 -threadedworker:1 -cluster:public -language:en -applet:/EE/Types/Framework/ContentUpdate
+    #$WINECMD "$EXEPREFIX/Warframe.x64.exe" -silent -log:/Preprocessing.log -dx10:1 -dx11:1 -threadedworker:1 -cluster:public -language:en -applet:/EE/Types/Framework/ContentUpdate
 
     echo "*********************"
     echo "Launching Warframe."
@@ -88,7 +108,7 @@ if [ "$WINEARCH" = "win64" ]; then
 
     $WINECMD "$EXEPREFIX/Warframe.x64.exe" -log:/Preprocessing.log -dx10:1 -dx11:1 -threadedworker:1 -cluster:public -language:en -fullscreen:0
 else
-    $WINECMD "$EXEPREFIX/Warframe.exe" -silent -log:/Preprocessing.log -dx10:1 -dx11:1 -threadedworker:1 -cluster:public -language:en -applet:/EE/Types/Framework/ContentUpdate
+    #$WINECMD "$EXEPREFIX/Warframe.exe" -silent -log:/Preprocessing.log -dx10:1 -dx11:1 -threadedworker:1 -cluster:public -language:en -applet:/EE/Types/Framework/ContentUpdate
 
     echo "*********************"
     echo "Launching Warframe."
