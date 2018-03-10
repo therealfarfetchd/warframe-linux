608759e4574bf83955942ae89862cbfa924fbd5b
the_real_farfetchd <git@marco-rebhan.de>
2018-03-10 22:16:28 +0100

    add dxvk support

--- install.sh
+++ install.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
 # Change to your preferred installation directory
-GAMEDIR="/home/${USER}/Games/Warframe"
-USE_DXVK=0
+GAMEDIR="${HOME}/Games/Warframe"
 
-SHARE="$HOME/.local/share"
+if which setup_dxvk64 > /dev/null 2> /dev/null
+then
+  USE_DXVK=1
+else
+  USE_DXVK=0
+fi
+#USE_DXVK=0 # force it to not use dxvk
+
+WTVERBS='vcrun2015 xact xinput win7'
+if [ $USE_DXVK -ne 0 ]
+then
+  WTVERBS='vcrun2015 win7'
+fi
+
+SHARE="${HOME}/.local/share"
 
 echo "*************************************************"
 echo "Creating wine prefix and performing winetricks."
 echo "*************************************************"
 
-WINEDEBUG=-all WINEARCH=win64 WINEPREFIX=$GAMEDIR winetricks -q vcrun2015 xact xinput win7
+WINEDEBUG=-all WINEARCH=win64 WINEPREFIX=$GAMEDIR winetricks -q $WTVERBS
+
+test $USE_DXVK -ne 0 && WINEDEBUG=-all WINEARCH=win64 WINEPREFIX=$GAMEDIR setup_dxvk64
 
 echo "*************************************************"
 echo "Creating warframe directories."
@@ -56,7 +71,7 @@ echo "WINEARCH=win64 WINEPREFIX=$GAMEDIR WINEDEBUG=-all bash updater.sh" >> warf
 
 
 chmod a+x warframe.sh
-cp ${GAMEDIR}/drive_c/Program\ Files/Warframe/warframe.sh "$HOME/bin/warframe"
+cp ${GAMEDIR}/drive_c/Program\ Files/Warframe/warframe.sh "${HOME}/bin/warframe"
 
 read -p "Would you like a menu shortcut? y/n " -n 1 -r
 echo    # (optional) move to a new line
@@ -66,24 +81,24 @@ then
 	echo "Creating warframe application menu shortcut."
 	echo "*************************************************"
 
-  mkdir -p "$SHARE/pixmaps/"
-  mkdir -p "$SHARE/applications/"
+  mkdir -p "${SHARE}/pixmaps/"
+  mkdir -p "${SHARE}/applications/"
 
-	cp warframe.png "$SHARE/pixmaps/"
+	cp warframe.png "${SHARE}/pixmaps/"
 
 	echo "[Desktop Entry]" > warframe.desktop
 	echo "Encoding=UTF-8" >> warframe.desktop
 	echo "Name=Warframe" >> warframe.desktop
 	echo "GenericName=Warframe" >> warframe.desktop
 	echo "Warframe" >> warframe.desktop
-	echo "Exec=$HOME/bin/warframe \"\$@\"" >> warframe.desktop
-	echo "Icon=$SHARE/pixmaps/warframe.png" >> warframe.desktop
+	echo "Exec=${HOME}/bin/warframe \"\$@\"" >> warframe.desktop
+	echo "Icon=${SHARE}/pixmaps/warframe.png" >> warframe.desktop
 	echo "StartupNotify=true" >> warframe.desktop
 	echo "Terminal=false" >> warframe.desktop
 	echo "Type=Application" >> warframe.desktop
 	echo "Categories=Application;Game" >> warframe.desktop
 
-	cp warframe.desktop "$SHARE/applications/"
+	cp warframe.desktop "${SHARE}/applications/"
 
   read -p "Would you like a desktop shortcut? y/n " -n 1 -r
   echo    # (optional) move to a new line
@@ -92,7 +107,7 @@ then
   	echo "*************************************************"
   	echo "Creating warframe desktop shortcut."
   	echo "*************************************************"
-  	cp "$SHARE/applications/warframe.desktop" "$HOME/Desktop/"
+  	cp "${SHARE}/applications/warframe.desktop" "${HOME}/Desktop/"
   fi
 fi
 
